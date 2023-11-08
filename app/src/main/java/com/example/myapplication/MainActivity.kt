c04b545add8e1b0e1055146ed9a6eefeea95ef8b
Ahamad Ali <2k22.cse.2213393@gmail.com>
2023-11-08 14:02:39 +0530

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,38 +11,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val loginButton = findViewById<Button>(R.id.buttonLogin)
+        val signUpButton = findViewById<Button>(R.id.buttonSignUp)
+
         loginButton.setOnClickListener {
-            // Replace the following placeholder logic with your actual authentication logic
-            val username = "user123"
-            val password = "password123"
-            val enteredUsername = findViewById<EditText>(R.id.editTextUsername).text.toString()
-            val enteredPassword = findViewById<EditText>(R.id.editTextPassword).text.toString()
+            // Handle login logic (validate credentials, etc.)
+            // For demonstration purposes, assume login is successful
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
 
-            if (enteredUsername == username && enteredPassword == password) {
-                // Authentication successful, redirect to HomeActivity
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-                finish() // Close the login activity to prevent going back on pressing back button
-            } else {
-                // Authentication failed, show error message (you can customize this part)
-                Toast.makeText(this, "Invalid credentials. Please try again.", Toast.LENGTH_SHORT)
-                    .show()
-            }
-                    val loginButton = findViewById<Button>(R.id.buttonLogin)
-                    val signUpButton = findViewById<Button>(R.id.buttonSignUp)
-
-                    loginButton.setOnClickListener {
-                        // Handle login logic (same as before)
-                    }
-
-                    signUpButton.setOnClickListener {
-                        // Redirect to SignUpActivity when Sign Up button is clicked
-                        val intent = Intent(this, SignUpActivity::class.java)
-                        startActivity(intent)
-                    }
-                }
-            }
-
+        signUpButton.setOnClickListener {
+            // Redirect to SignUpActivity when Sign Up button is clicked
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 }
